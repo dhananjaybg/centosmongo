2f7eff3b0e8bf54afe25418d6818cf20bd451374
Dhananjay Ghevde <dhananjaybg@gmail.com>
2021-08-09 21:54:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
 sudo yum -y update
+sudo yum -y python3
+sudo pip3 install pymongo
+sudo pip3 install dnspython
+sudo pip3 list
+cp mongodb-org-5.0.repo  /etc/yum.repos.d/mongodb-org-5.0.repo
+sudo yum install -y mongodb-mongosh
