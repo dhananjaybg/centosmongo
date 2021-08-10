c39b1f70b622c174553ea68b396e5347b2abce42
Dhananjay Ghevde <dhananjaybg@gmail.com>
2021-08-10 15:12:24 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 sudo yum -y update
-sudo yum -y python3
+sudo yum install python3
 sudo pip3 install pymongo
 sudo pip3 install dnspython
+sudo pip3 install faker
 sudo pip3 list
-cp mongodb-org-5.0.repo  /etc/yum.repos.d/mongodb-org-5.0.repo
+sudo cp mongodb-org-5.0.repo  /etc/yum.repos.d/mongodb-org-5.0.repo
 sudo yum install -y mongodb-mongosh
